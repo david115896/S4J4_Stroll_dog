ae7e2be30cab40a68cf44f9c6f97c2c22773df7b
David.T <50680727+david115896@users.noreply.github.com>
2019-10-25 12:12:56 +0200

    Update dog.rb

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,5 @@
 class Dog < ApplicationRecord
 	has_many :strolls		#Dog est en relation N-1 avec City et en relation N-N avec Dogsitter
-	has_many :dogsitter, through: :strolls
+	has_many :dogsitters, through: :strolls
 	belongs_to :city
 end
